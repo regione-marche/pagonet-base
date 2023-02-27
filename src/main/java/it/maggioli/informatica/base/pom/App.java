befd37da02e762f2256fb260c4006df09bfdfaea
lorenzo.rava <lorenzo.rava@injenia.it>
2023-02-27 11:18:52 +0100

    Initial commit

---
+++ src/main/java/it/maggioli/informatica/base/pom/App.java
@@ -0,0 +1,13 @@
+package it.maggioli.informatica.base.pom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/it/maggioli/informatica/base/pom/AppTest.java
@@ -0,0 +1,20 @@
+package it.maggioli.informatica.base.pom;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
